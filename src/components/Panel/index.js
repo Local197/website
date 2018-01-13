4bfeecc184b74f76b5437488fa2abc0268c7ba88
Jose Canahui <jose.canahui@gmail.com>
2018-01-12 22:54:39 -0500

    Populated homepage

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import Translate from 'containers/Translate';
+
 require('./index.scss');
 
 export default class Panel extends Component {
@@ -12,7 +14,12 @@ export default class Panel extends Component {
         </div>
         { this.props.noSidebar == true ? null :
           <div className="app-Panel-sidebar">
-            <p>video</p>
+            <Translate language="en">
+              <iframe width="100%" height="220" src="https://www.youtube.com/embed/3-X5hCxdaLc" frameBorder="0" allowFullScreen></iframe>
+            </Translate>
+            <Translate language="es">
+              <iframe width="100%" height="220" src="https://www.youtube.com/embed/iaBCBXy600E" frameBorder="0" allowFullScreen></iframe>
+            </Translate>
             <p>join our movement</p>
             <p>facebook</p>
             <p>blog</p>
