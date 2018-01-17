6704237d0d6b422210c4144fa1543e7dd1299917
Jose Canahui <jose.canahui@gmail.com>
2018-01-16 23:30:06 -0500

    Added spanish for training and sign up views

--- src/views/SignUp/index.js
+++ src/views/SignUp/index.js
@@ -16,14 +16,51 @@ export default class SignUpView extends Component {
           <div className="app-SignUpView-container">
             <Text>
               <h2>
-                <Translate language="en">Sign Up to Local 197 Online</Translate>
+                <Translate language="en">Sign Up to Local 197</Translate>
+                <Translate language="es">Reg&iacute;strarse a Local 197</Translate>
               </h2>
-              <p>You can join the Local by</p>
-              <p>After joining, you will be given a union ID number. You can join the Local 197 online by signing up with that numberhere. By joining you will be able to access the following:</p>
+              <p>
+                <Translate language="en">
+                  To make an appointment to join Local 197, you can call 301-735-6660 and ask for an interview. They are open Monday - Friday, 7:00AM - 4:00PM.
+                </Translate>
+                <Translate language="es">
+                  Aplicables a la uni&oacute;n deber&iacute;an ir en persona a la oficina para una entrevista. La entrevista se consigue por tel&eacute;fono, llamando al 301-735-6660. Hablar con un representante del consejo o dejar un mensaje espec&iacute;fico. <br/> <br/> El horario es Lunes - Viernes de las 7:00AM - 4:00PM.
+                </Translate>
+              </p>
+
+              <p>
+                <Translate language="en">
+                  After joining, you will be given a union ID number. You can join the Local 197 online by signing up with that numberhere. By joining you will be able to access the following:
+                </Translate>
+                <Translate language="es">
+                  Despu&eacute;s de unirse, se le dar&aacute; un n&uacute;mero de identificaci&oacute;n del sindicato. Puede unirse al Local 197 en l&iacute;nea registr&aacute;ndose con ese n&uacute;mero aqu&iacute;. Al unirse, podr&aacute; acceder a lo siguiente:
+                </Translate>
+              </p>
               <ul>
-                <li><b>Find a job:</b> we keep an updated list of avaliable union jobs.</li>
-                <li><b>Union benefits:</b> quick access to the benefits you have as a union member.</li>
-                <li><b>Report jobs (coming soon):</b> let your union brothers know of avaiable jobs and of union-compliant jobs.</li>
+                <li>
+                  <Translate language="en">
+                    <b>Find a job:</b> we keep an updated list of avaliable union jobs.
+                  </Translate>
+                  <Translate language="es">
+                    <b>Encuentra un trabajo:</b> mantenemos una lista actual de trabajos de union disponibles.
+                  </Translate>
+                </li>
+                <li>
+                  <Translate language="en">
+                    <b>Union benefits:</b> quick access to the benefits you have as a union member.
+                  </Translate>
+                  <Translate language="es">
+                    <b>Beneficios de union:</b> acceso r&aacute;pido a beneficios que tiene como miembro de la union.
+                  </Translate>
+                </li>
+                <li>
+                  <Translate language="en">
+                    <b>Report jobs (coming soon):</b> let your union brothers know of avaiable jobs and of union-compliant jobs.
+                  </Translate>
+                  <Translate language="es">
+                    <b>Reportar trabajos (biene pronto):</b> ayuda a sus hermanos de la union compartiendo informaci&oacute;n sobre trabajos de union.
+                  </Translate>
+                </li>
               </ul>
             </Text>
             <SignUp />
--- src/views/Training/index.js
+++ src/views/Training/index.js
@@ -25,60 +25,102 @@ export default class TrainingView extends Component {
               <Translate language="en">
                 Why training?
               </Translate>
+              <Translate language="es">
+                Porque entrenarse?
+              </Translate>
             </h4>
             <p>
               <Translate language="en">
                 Be the best you can be. Strengthen your knowledge and be more employable and more competitive, by keeping up with your qualifications and certifications.
+              </Translate>
+              <Translate language="es">
+                Dest&aacute;cate. Fortalece tus conocimientos y abre oportunidades de empleo y competencia, manteniendo tus certificaciones y calificaciones al d&iacute;a.
               </Translate>
             </p>
             <h4>
               <Translate language="en">
                 Free classes
               </Translate>
+              <Translate language="es">
+                Clases Gratis
+              </Translate>
             </h4>
             <p>
               <Translate language="en">
                 Classes are at not cost to you, but offered only to union members. Please keep your card current. You will be issued full color textbooks and will be taught at top of the line installations with highly qualified instructors. We are aiming to best serve the industry needs.
+              </Translate>
+              <Translate language="es">
+                Clases son sin costo para ti, pero solo se ofrecen a miembros de uni&oacute;n. Favor de mantener su tarjeta vigente. Se te dar&aacute;n libros a todo color y se te ense&ntilde;ara en instalaciones modernas con instructores calificados. Estamos mirando en servir mejor las necesidades de la industria.
               </Translate>
             </p>
             <h4>
               <Translate language="en">
                 How to register
               </Translate>
+              <Translate language="es">
+                Como Registrarse
+              </Translate>
             </h4>
             <p>
               <Translate lanugage="en">
                 They are classes designed for you, conveniently set after working hours. See which one fits your schedule and call the school to reserve a seat.
               </Translate>
+              <Translate language="es">
+                Tenemos clases dise&ntilde;adas para ti, en horas convenientes. Ve cual se ajusta a tu calendario y llama a la escuela para que reserves un asiento.
+              </Translate>
             </p>
             <p>
               <Translate lanugage="en">
-                Telephone: (301)736-1696
+                Telephone:
               </Translate>
+              <Translate language="es">
+                Tel&eacute;fono:
+              </Translate>
+              (301)736-1696
             </p>
             <p>
               <Translate language="en">
                 Check out what classes we offer: <a href="https://www.197-carpenters.com/JUS.pdf" target="__blank">Classes.pdf</a>
               </Translate>
+              <Translate language="es">
+                Estas son las clases que damos:
+              </Translate>
+              <a href="https://www.197-carpenters.com/JUS.pdf" target="__blank">
+                <Translate language="en">Classes.pdf</Translate>
+                <Translate language="es">Clases.pdf</Translate>
+              </a>
             </p>
             <p>
               <Translate language="en">
-                Carpenters’ Training Center: 8510 Pennsylvania Ave. Upper Marlboro MD 201772
+                Carpenters’ Training Center:
               </Translate>
+              <Translate language="es">
+                Centro de Entrenamiento de Carpinteros:
+              </Translate>
+              8510 Pennsylvania Ave. Upper Marlboro MD 201772
             </p>
             <h2>
               <Translate language="en">
                 UBC Training Verification Card
+              </Translate>
+              <Translate language="es">
+                UBC Tarjeta de Verificaci&oacute;n de Entrenamiento
               </Translate>
             </h2>
             <p>
               <Translate language="en">
                 The Carpenter International Training Fund has begun sending members a Personal Training Verification Card (TVC). The purpose of this card is to act as a single source to verify all of our members certifications. Simply scan the QR Code on the face of the card with one of the many barcode apps available for smart phones. You will then be taken to a personal webpage listing all of your certifications with the Labor Technical College.
               </Translate>
+              <Translate language="es">
+                El Fondo de Entrenamiento Internacional de Carpinteros ha comenzado a enviar a miembros  una Tarjeta Personal de Verificaci&oacute;n de Entrenamiento (TVC). El prop&oacute;sito de esta tarjeta es que sirva como vehículo &uacute;nico para verificar todas las certificaciones del miembro. Simplemente escanea el C&oacute;digo QR de encima de la tarjeta usando uno de los muchos dispositivos de aplicaciones disponibles en los tel&eacute;fonos inteligentes. Este te llevara a una pagina personal de internet la cual listara todas las certificaciones obtenidas con el Colegio T&eacute;cnico del Trabajo.
+              </Translate>
             </p>
             <p>
               <Translate language="en">
                 Important: In order to receive this TVC card, you will have to take any certification or qualification class first, and your union card has to be current. Pictures are taken at a training center near you. GET YOURS NOW!
+              </Translate>
+              <Translate language="es">
+                Importante: Para poder recibir esta tarjeta TVC, deber&aacute;s tomar primero clases de certificaci&oacute;n o calificaci&oacute;n, y tu tarjeta de uni&oacute;n deber&aacute; estar al corriente. Tu foto puede tomarse en uno de los centros de entrenamiento mas cercano a ti.  SACA TU CREDENCIAL AHORA!
               </Translate>
             </p>
             <img className="app-TrainingView-image" src={require('assets/training.png')} />
